eac9df3b64c949acdce4a7c64b98578056271379
manish mandal <76776194mm@gmail.com>
2024-02-07 19:30:03 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log('Connected to MONGODB');
+}).catch((err) => {
+    console.log(err);
+});
 
 app.listen(3001, () => {
     console.log('serving running on port 3000...');
