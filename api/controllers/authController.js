65f7f924fb21a12d4789222b429f66c28994fdb2
manish mandal <76776194mm@gmail.com>
2024-02-09 01:50:56 +0530

    create sign in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
 const bcryptjs = require('bcryptjs');
 const User = require("../models/userModel");
+const errorHandler = require('../utils/error');
+const jwt = require('jsonwebtoken');
 
 exports.signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -8,6 +10,27 @@ exports.signup = async (req, res, next) => {
     try {
         await newUser.save();
         res.status(201).json('User created successfully');
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email }).select('+password');
+
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+        validUser.password = undefined;
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(validUser);
     } catch (error) {
         next(error);
     }
