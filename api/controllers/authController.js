c7a6112e0f2de23a34b170cf421fb69422406a47
manish mandal <76776194mm@gmail.com>
2024-02-11 11:56:18 +0530

    complete profile page ui

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -46,13 +46,11 @@ exports.google = async (req, res, next) => {
         } else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.imageUrl, avatar: req.body.photo });
+            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo });
             await newUser.save();
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
             newUser.password = undefined;
             res.cookie('access_token', token, { httpOnly: true }).status(200).json(newUser);
-
-
         }
     } catch (error) {
         next(error);
